406ba30e5e0ab8f348179e9720aa86ab0c21e702
Haiul Sk <HaiulSk037@gmail.com>
2025-08-10 16:06:01 +0530

    Components

--- Client/src/components/CourseCard.jsx
+++ Client/src/components/CourseCard.jsx
@@ -4,12 +4,12 @@ import { Button } from './ui/button'
 
 const CourseCard = ({course}) => {
   return (
-    <Card className="bg-white shadow-lg">
+    <Card className="bg-white border-none w-64 h-75">
         <img src={course.image} alt=""  className='w-full h-30 object-cover'/>
-        <div className='p-3'>
-            <h2 className='text-xl font-semibold text-gray-800 mb-1'>{course.title}</h2>
-            <p className='text-gray-600 mb-2'>{course.description}</p>
-            <Button>Learn More</Button>
+        <div>
+            <h2 className='text-xl font-semibold text-gray-800 mb-1 truncate'>{course.title}</h2>
+            <p className='text-gray-600 mb-1 line-clamp-2'>{course.description}</p>
+            <Button className="bg-blue-300">Learn More</Button>
         </div>
     </Card>
   )
