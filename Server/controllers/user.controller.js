b24316dab2071f77981642993a45b9d8c85729e7
Haiul Sk <HaiulSk037@Gamil.com>
2025-08-10 11:00:20 +0530

    UserController

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,5 +1,7 @@
+import { json } from "express";
 import { User } from "../models/user.models.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 
 export const register = async (req,res) => {
     try{
@@ -39,6 +41,61 @@ export const register = async (req,res) => {
         res.status(500).json({
             success:false,
             message:"failed to register"
+        })
+    }
+}
+
+export const login = async(req, res) => {
+    try {
+        const {email, password} = req.body
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            })
+        }
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect email or password"
+            })
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect email or password"
+            })
+        }
+        // generate token
+        const token = jwt.sign({userId:user._id}, process.env.SECRET_KEY, {expiresIn: "1d"} )
+        return res.cookie('token', token, {httpOnly:true, sameSite:'strict', maxAge:1*24*60*60*1000}).json({
+            message: `Welcome back ${user.name}`,
+            success:true,
+            user,
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to Login"
+        })
+    }
+}
+
+export const logout = async (__,res)=>{
+    try{
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"Logout succesfully",
+            success:true
+        })
+    }catch(err){
+        console.log(err)
+        return res.status(400).json({
+             success:true,
+             message:"Logout successfully"
         })
     }
 }
