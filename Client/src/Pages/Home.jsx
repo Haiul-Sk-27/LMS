a2bc014ac41cbdd8768a7ce1830c3d08c35abb41
Haiul Sk <HaiulSk037@gmail.com>
2025-08-10 20:47:03 +0530

    Login

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Hero />
       <div><h1 className='text-4xl font-bold text-center text-gray-800 mb-3'>Our Courses</h1>
         <p className='text-center text-gray-600 mb-2'>Explore our curated courses to boost your skills and career. Whether you're a beginner or an expert, we have something for everyone</p></div>
-      <div className='max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+      <div className='max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6'>
         {
           coursesJson.map((course) => {
           return <CourseCard course={course} />
--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,46 +1,67 @@
-import { GraduationCap } from 'lucide-react'
 import React from 'react'
-import { Button } from './ui/button'
+import { GraduationCap } from 'lucide-react';
+import { Button } from './ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { setUser } from '@/redux/authSlice';
+import { toast } from 'sonner';
 
 const Navbar = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { user } = useSelector(store => store.auth)
 
-    const user = false;
+    const logoutHandler = async (e) => {
+        try {
+            const res = await axios.get('https://lms-nswg.onrender.com/api/v1/user/logout', {withCredentials:true});
+            if(res.data.success){
+                navigate('/')
+                dispatch(setUser(null))
+                toast.success(res.data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message)
+        }
+    }
     return (
-        <div className='bg-gray-900 z-50 w-full fixed top-0 p-2'>
+        <div className='bg-gray-900 z-50 w-full py-3 fixed top-0'>
             <div className='max-w-7xl mx-auto flex justify-between'>
-                {/*Logo Section*/}
+                {/* logo section */}
                 <Link to='/'>
-                    <div className='flex'>
-                    <GraduationCap className='text-gray-300 w-10 h-10' />
-                    <h1 className='text-gray-300 font-bold text-3xl'>Logo</h1>
-                </div>
+                    <div className='flex gap-1'>
+                        <GraduationCap className='text-gray-300 w-10 h-10' />
+                        <h1 className='text-gray-300 text-3xl font-bold '>Logo</h1>
+                    </div>
                 </Link>
 
-                {/*Menu Section*/}
+                {/* menu section */}
                 <nav>
                     <ul className='flex gap-7 text-xl items-center font-semibold text-white'>
-                        <Link to='/'>Home</Link>
-                        <Link to='/course'>Course</Link>
+                        <Link to="/"><li className='cursor-pointer'>Home</li></Link>
+                        <Link to="/courses"><li className='cursor-pointer'>Courses</li></Link>
+
                         {
                             !user ? (
                                 <div className='flex gap-3'>
-                                    <Link to='/login'><Button className='bg-blue-500 hover:bg-blue-600'>Login</Button></Link>
-                                    <Link to='/signup'><Button className="bg-gray-700 hover:bg-gray-800">SignUp</Button></Link>
+                                    <Link to="/login"><Button className="bg-blue-500 hover:bg-blue-600">Login</Button></Link>
+                                    <Link to="/signup"><Button className="bg-gray-700 hover:bg-gray-800">Signup</Button></Link>
                                 </div>
                             ) : (
                                 <div className='flex items-center gap-7'>
-                                    <Avatar>
-                                        <AvatarImage
-                                            src="https://github.com/shadcn.png"
-                                            alt="@evilrabbit"
-                                        />
-                                        <AvatarFallback>ER</AvatarFallback>
-                                    </Avatar>
+                                    {
+                                        user.role === "instructor" && <Link to="/admin/dashboard"><li className='cursor-pointer'>Admin</li></Link>
+                                    }
+                                    <Link to="/profile">
+                                        <Avatar>
+                                            <AvatarImage src={user.photoUrl} alt="@shadcn" />
+                                            <AvatarFallback>CN</AvatarFallback>
+                                        </Avatar>
+                                    </Link>
 
-                                    <Button className="bg-blue-500 hover:bg-blue-600">Logout</Button>
+                                    <Button onClick={logoutHandler} className="bg-blue-500 hover:bg-blue-600">Logout</Button>
                                 </div>
                             )
                         }
