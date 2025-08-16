913f3b6ba41519d9842ed1e3dbbf2a94b09075d6
Haiul sk <HaiulSk037@Gmail.com>
2025-08-16 18:05:55 +0530

    LMS

--- Server/controllers/course.controller.js
+++ Server/controllers/course.controller.js
@@ -120,3 +120,28 @@ export const editCourse = async (req, res) => {
   }
 };
 
+export const getCourseById = async(req,res) => {
+    try{
+        const {courseId} = req.params;
+        const course = await Course.findById(courseId);
+
+        if(!course){
+            return res.status(404).json({
+                success:false,
+                message:"course not found"
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            course
+        })
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            success:false,
+            message:"Failed to course load"
+        })
+    }
+}
+
--- Server/routes/course.route.js
+++ Server/routes/course.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAuthenticated } from '../middleware/isAuthenticated.js';
-import { createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getPublishedCourse, removeLecture, togglePublishedCourse } from '../controllers/course.controller.js';
+import { createCourse, editCourse,  getCourseById, getCreatorCourses, getPublishedCourse} from '../controllers/course.controller.js';
 import uploadProfilePic from '../middleware/multer.js';
 
 const router = express.Router();
@@ -9,6 +9,6 @@ router.post('/',isAuthenticated,createCourse);
 router.get('/published-courses',getPublishedCourse);
 router.get('/',isAuthenticated,getCreatorCourses);
 router.put("/course/:courseId", uploadProfilePic.single("courseThumbnail"), editCourse);
-
+router.get("/:courseId",isAuthenticated,getCourseById);
 
 export default router
