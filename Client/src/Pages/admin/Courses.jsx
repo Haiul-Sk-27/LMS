d8d87b5744c8793971ab094fe85b2215e4946978
Haiul sk <HaiulSk037@Gmail.com>
2025-09-10 22:06:15 +0530

    Courses

--- Client/src/Pages/admin/Courses.jsx
+++ Client/src/Pages/admin/Courses.jsx
@@ -69,7 +69,7 @@ const Course = () => {
     useEffect(() => {
         const getCreatorCourse = async () => {
             try {
-                const res = await axios.get('http://localhost:3002/api/v1/course/', { withCredentials: true })
+                const res = await axios.get('https://lms-yuq1.onrender.com/api/v1/course/', { withCredentials: true })
                 if (res.data.success) {
                     dispatch(setCourse(res.data.courses))
                 }
